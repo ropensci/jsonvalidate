0f2782aefc4a6ade7e21f00528f031e8c1c6d1ce
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-03-31 16:57:22 -0700

    Add a verbose option

--- R/validate.R
+++ R/validate.R
@@ -7,8 +7,12 @@
 json_validator <- function(schema) {
   name <- basename(tempfile("jv_"))
   env$ct$eval(sprintf("%s = validator(%s)", name, get_string(schema)))
-  ret <- function(json) {
-    env$ct$call(name, V8::JS(get_string(json)))
+  ret <- function(json, verbose=FALSE) {
+    res <- env$ct$call(name, V8::JS(get_string(json)), list(verbose=verbose))
+    if (verbose) {
+      attr(res, "errors") <- env$ct$get(paste0(name, ".errors"))
+    }
+    res
   }
   attr(ret, "name") <- name
   ret
@@ -19,11 +23,13 @@ json_validator <- function(schema) {
 ##' @title Validate a json file
 ##' @inheritParams json_validator
 ##' @param json Contents of a json object, or a filename containing one.
+##' @param verbose Be verbose?  If \code{TRUE}, then an attribute
+##'   "errors" will list validation failures as a data.frame
 ##' @export
-json_validate <- function(json, schema) {
+json_validate <- function(json, schema, verbose=FALSE) {
   tmp <- json_validator(schema)
   on.exit(env$ct$eval(sprintf("delete %s", attr(tmp, "name"))))
-  tmp(json)
+  tmp(json, verbose)
 }
 
 get_string <- function(x) {
