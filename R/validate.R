da093d29a18fb81e275e984167177868a12a8849
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-03-31 17:01:08 -0700

    Add a greedy mode

--- R/validate.R
+++ R/validate.R
@@ -7,8 +7,10 @@
 json_validator <- function(schema) {
   name <- basename(tempfile("jv_"))
   env$ct$eval(sprintf("%s = validator(%s)", name, get_string(schema)))
-  ret <- function(json, verbose=FALSE) {
-    res <- env$ct$call(name, V8::JS(get_string(json)), list(verbose=verbose))
+  ret <- function(json, verbose=FALSE, greedy=FALSE) {
+    res <- env$ct$call(name, V8::JS(get_string(json)),
+                       list(verbose=verbose),
+                       list(greedy=greedy))
     if (verbose) {
       attr(res, "errors") <- env$ct$get(paste0(name, ".errors"))
     }
@@ -25,11 +27,12 @@ json_validator <- function(schema) {
 ##' @param json Contents of a json object, or a filename containing one.
 ##' @param verbose Be verbose?  If \code{TRUE}, then an attribute
 ##'   "errors" will list validation failures as a data.frame
+##' @param greedy Continue after the first error?
 ##' @export
-json_validate <- function(json, schema, verbose=FALSE) {
+json_validate <- function(json, schema, verbose=FALSE, greedy=FALSE) {
   tmp <- json_validator(schema)
   on.exit(env$ct$eval(sprintf("delete %s", attr(tmp, "name"))))
-  tmp(json, verbose)
+  tmp(json, verbose, greedy)
 }
 
 get_string <- function(x) {
