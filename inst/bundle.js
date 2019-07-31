b5cbd31ba5ab87d113d93aad57fd05812beb801f
Rich FitzJohn <rich.fitzjohn@gmail.com>
2019-07-31 08:21:38 +0100

    Backport changes from 760d970 / #16
    This should have been added to the in.js file rather than directly
    to the bundle file - we should document this better

--- inst/bundle.js
+++ inst/bundle.js
@@ -1,20 +1,20 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 (function (global){
 global.Ajv = require('ajv');
 global.AjvSchema4 = require('ajv/lib/refs/json-schema-draft-04.json');
 global.AjvSchema6 = require('ajv/lib/refs/json-schema-draft-06.json');
 
 global.ajv = new Ajv({allErrors: true, verbose: true})
-                   .addMetaSchema(AjvSchema6);
+    .addMetaSchema(AjvSchema6);
 
 global.ajv_04 = new Ajv({meta: false, schemaId: 'id', allErrors: true, verbose: true})
-                     .addMetaSchema(AjvSchema4)
-                     .removeKeyword('propertyNames')
-                     .removeKeyword('contains')
-                     .removeKeyword('const')
-                     .removeKeyword('if')
-                     .removeKeyword('then')
-                     .removeKeyword('else');
+    .addMetaSchema(AjvSchema4)
+    .removeKeyword('propertyNames')
+    .removeKeyword('contains')
+    .removeKeyword('const')
+    .removeKeyword('if')
+    .removeKeyword('then')
+    .removeKeyword('else');
 
 global.get_meta_schema = function(schema) {
   return schema.$schema;
@@ -5281,7 +5281,7 @@ module.exports={
         "$data": {
             "type": "string",
             "anyOf": [
-                { "format": "relative-json-pointer" },
+                { "format": "relative-json-pointer" }, 
                 { "format": "json-pointer" }
             ]
         }
@@ -8332,14 +8332,103 @@ if (typeof Object.create === 'function') {
 
 },{}],58:[function(require,module,exports){
 // shim for using process in browser
-
 var process = module.exports = {};
+
+// cached from whatever global is present so that test runners that stub it
+// don't break things.  But we need to wrap it in a try catch in case it is
+// wrapped in strict mode code which doesn't define any globals.  It's inside a
+// function because try/catches deoptimize in certain engines.
+
+var cachedSetTimeout;
+var cachedClearTimeout;
+
+function defaultSetTimout() {
+    throw new Error('setTimeout has not been defined');
+}
+function defaultClearTimeout () {
+    throw new Error('clearTimeout has not been defined');
+}
+(function () {
+    try {
+        if (typeof setTimeout === 'function') {
+            cachedSetTimeout = setTimeout;
+        } else {
+            cachedSetTimeout = defaultSetTimout;
+        }
+    } catch (e) {
+        cachedSetTimeout = defaultSetTimout;
+    }
+    try {
+        if (typeof clearTimeout === 'function') {
+            cachedClearTimeout = clearTimeout;
+        } else {
+            cachedClearTimeout = defaultClearTimeout;
+        }
+    } catch (e) {
+        cachedClearTimeout = defaultClearTimeout;
+    }
+} ())
+function runTimeout(fun) {
+    if (cachedSetTimeout === setTimeout) {
+        //normal enviroments in sane situations
+        return setTimeout(fun, 0);
+    }
+    // if setTimeout wasn't available but was latter defined
+    if ((cachedSetTimeout === defaultSetTimout || !cachedSetTimeout) && setTimeout) {
+        cachedSetTimeout = setTimeout;
+        return setTimeout(fun, 0);
+    }
+    try {
+        // when when somebody has screwed with setTimeout but no I.E. maddness
+        return cachedSetTimeout(fun, 0);
+    } catch(e){
+        try {
+            // When we are in I.E. but the script has been evaled so I.E. doesn't trust the global object when called normally
+            return cachedSetTimeout.call(null, fun, 0);
+        } catch(e){
+            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error
+            return cachedSetTimeout.call(this, fun, 0);
+        }
+    }
+
+
+}
+function runClearTimeout(marker) {
+    if (cachedClearTimeout === clearTimeout) {
+        //normal enviroments in sane situations
+        return clearTimeout(marker);
+    }
+    // if clearTimeout wasn't available but was latter defined
+    if ((cachedClearTimeout === defaultClearTimeout || !cachedClearTimeout) && clearTimeout) {
+        cachedClearTimeout = clearTimeout;
+        return clearTimeout(marker);
+    }
+    try {
+        // when when somebody has screwed with setTimeout but no I.E. maddness
+        return cachedClearTimeout(marker);
+    } catch (e){
+        try {
+            // When we are in I.E. but the script has been evaled so I.E. doesn't  trust the global object when called normally
+            return cachedClearTimeout.call(null, marker);
+        } catch (e){
+            // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error.
+            // Some versions of I.E. have different rules for clearTimeout vs setTimeout
+            return cachedClearTimeout.call(this, marker);
+        }
+    }
+
+
+
+}
 var queue = [];
 var draining = false;
 var currentQueue;
 var queueIndex = -1;
 
 function cleanUpNextTick() {
+    if (!draining || !currentQueue) {
+        return;
+    }
     draining = false;
     if (currentQueue.length) {
         queue = currentQueue.concat(queue);
@@ -8355,7 +8444,7 @@ function drainQueue() {
     if (draining) {
         return;
     }
-    var timeout = setTimeout(cleanUpNextTick);
+    var timeout = runTimeout(cleanUpNextTick);
     draining = true;
 
     var len = queue.length;
@@ -8372,7 +8461,7 @@ function drainQueue() {
     }
     currentQueue = null;
     draining = false;
-    clearTimeout(timeout);
+    runClearTimeout(timeout);
 }
 
 process.nextTick = function (fun) {
@@ -8384,7 +8473,7 @@ process.nextTick = function (fun) {
     }
     queue.push(new Item(fun, args));
     if (queue.length === 1 && !draining) {
-        setTimeout(drainQueue, 0);
+        runTimeout(drainQueue);
     }
 };
 
@@ -8412,6 +8501,10 @@ process.off = noop;
 process.removeListener = noop;
 process.removeAllListeners = noop;
 process.emit = noop;
+process.prependListener = noop;
+process.prependOnceListener = noop;
+
+process.listeners = function (name) { return [] }
 
 process.binding = function (name) {
     throw new Error('process.binding is not supported');
--- js/in.js
+++ js/in.js
@@ -2,9 +2,20 @@ global.Ajv = require('ajv');
 global.AjvSchema4 = require('ajv/lib/refs/json-schema-draft-04.json');
 global.AjvSchema6 = require('ajv/lib/refs/json-schema-draft-06.json');
 
-global.AjvGenerator =
-    new Ajv({allErrors: true, schemaId: 'auto', verbose: true});
-global.AjvGenerator.addMetaSchema(AjvSchema4);
-global.AjvGenerator.addMetaSchema(AjvSchema6);
+global.ajv = new Ajv({allErrors: true, verbose: true})
+    .addMetaSchema(AjvSchema6);
+
+global.ajv_04 = new Ajv({meta: false, schemaId: 'id', allErrors: true, verbose: true})
+    .addMetaSchema(AjvSchema4)
+    .removeKeyword('propertyNames')
+    .removeKeyword('contains')
+    .removeKeyword('const')
+    .removeKeyword('if')
+    .removeKeyword('then')
+    .removeKeyword('else');
+
+global.get_meta_schema = function(schema) {
+  return schema.$schema;
+};
 
 global.imjv = require('is-my-json-valid');
